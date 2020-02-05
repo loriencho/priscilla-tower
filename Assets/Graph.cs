e6cf02c72e5a18a68913475ac09b0802c2defd86
Lorien Cho <lorien.yuna.cho@gmail.com>
2020-02-05 10:38:52 -0500

    First commit, adding Unity files

---
+++ Assets/Graph.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform projectilePrefab;
+
+    void Awake() {
+
+        for (int i = 0; i < 10; i++) {
+            Transform projectile = Instantiate(projectilePrefab);
+            projectile.localPosition = Vector2.right * i;
+        }
+    }
+}
